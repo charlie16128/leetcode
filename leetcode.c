dcc0d04a5de2e11db8051d853dc0027d604dff6b
charlie16128 <ctugm24@gmail.com>
2025-12-05 02:58:18 +0800

    first commit

---
+++ leetcode.c
@@ -0,0 +1,50 @@
+﻿#include <stdio.h>
+#include <string.h>
+
+char* longestCommonPrefix(char** strs, int strsSize) {
+    char *prefix = strs[0];
+    for(int i = 1; i < strsSize; i++){
+        while(strstr(strs[i], prefix) != strs[i]){
+            prefix[strlen(prefix) - 1] = '\0';
+            if(!*prefix) return "";
+        }
+    }
+    return prefix;
+}
+
+char* Mystrstr(char* str1, char* str2){
+    if(*str2 == '\0') return str1;
+
+    for(; *str1 == '\0'; str1++){   
+        char *p1 = str1;
+        char *p2 = str2;
+        // compare str1 str2
+        while(*p1 && *p2 && *p1 == *p2){
+            p1++;
+            p2++;
+        }
+        if(*p2 == '\0') return str1;
+    }
+}
+
+int main(){
+    char str1[100], str2[100];
+    scanf("%s", str1);
+    scanf("%s", str2);
+    return 0;
+}
+
+char* Mystrstr(char* str1, char* str2){
+    if (!*str2) return str1;
+
+    for (; *str1; str1++){
+        char *p1 = str1;
+        char *p2 = str2;
+        while (*p1 && *p2 && *p1 == *p2){
+            p1++;
+            p2++;
+        }
+        if (!*p2) return str1;
+    }
+    return NULL;
+}
---
+++ leetcode20.c
@@ -0,0 +1,21 @@
+﻿bool isValid(char* s) {
+    int len = strlen(s);
+    char stack[len]; 
+    int top = -1;
+
+    for(int i = 0; i < len; i++){
+        char c = s[i];
+
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            stack[++top] = s[i];
+        }
+        else{
+            if(top == -1) return false;
+            char p = stack[top--]
+            if((c == ')' && p != '(') || (c == ']' && p != '[') || (c == '}' && p != '{')){
+                return false;
+            }
+        }
+    }
+    return true;
+}
