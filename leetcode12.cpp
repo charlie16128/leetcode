085c4df3a16618c782b3d55267cc4984febf2646
Charlie Tu <113664602+charlie16128@users.noreply.github.com>
2025-12-11 14:35:45 +0800

    Add files via upload

---
+++ leetcode12.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        string intToRoman(int num) {
+            const vector<pair<int, string>> valueSymbols = {
+                {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+                {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+                {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+            };
+
+            string result = "";
+
+            for(const auto& pair : valueSymbols){
+                while(num >= pair.first){
+                    num -= pair.first;
+                    result += pair.second;
+                }
+                if(num == 0) break;
+            }
+            return result;
+        }
+};
+
+int main(){}
